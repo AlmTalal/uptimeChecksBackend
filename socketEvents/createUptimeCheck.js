2967b34d18ce0b29d96344c577a453cfa29c7026
Talal <alamtalal004@gmail.com>
2023-08-11 19:53:05 -0400

    Added the create Uptime Check event handler

---
+++ socketEvents/createUptimeCheck.js
@@ -0,0 +1,27 @@
+const { validateUptimeCheck, UptimeCheck } = require("../models/uptimeCheck");
+
+module.exports = (socket) => {
+  socket.on("createUptimeCheck", async (newUptimeCheck) => {
+    //Validates the newUptimeCheck
+    const error = validateUptimeCheck(newUptimeCheck);
+    //If there is an error it emit it an return
+    if (error) {
+      socket.emit("CreateUptimeCheckStatus", error.details[0].message);
+      return;
+    }
+
+    //It tries to create an uptime check, if it's succesfull emits true else emits false
+    try {
+      const uptimeCheck = new UptimeCheck({
+        chekedUrl: newUptimeCheck.chekedUrl,
+        checkedId: newUptimeCheck.checkedId,
+        name: newUptimeCheck.name,
+        checkedStatus: newUptimeCheck.checkedStatus,
+      });
+      await uptimeCheck.save();
+      socket.emit("CreateUptimeCheckStatus", true);
+    } catch (error) {
+      socket.emit("CreateUptimeCheckStatus", false);
+    }
+  });
+};
