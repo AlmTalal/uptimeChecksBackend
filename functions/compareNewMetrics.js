1342fc7ae9ee6d79f69270b4520642ddb3d2586e
Talal <alamtalal004@gmail.com>
2023-08-12 03:16:15 -0400

    Added a try catch block

--- functions/compareNewMetrics.js
+++ functions/compareNewMetrics.js
@@ -10,22 +10,26 @@ module.exports = async (newMetrics) => {
 
   for (metric of newMetrics) {
     //We find its metric instance by the metric id
-    const currentMetricStatus = await UptimeCheck.find({
-      checkedId: metric[0],
-    });
-    //if there is an instance
-    if (currentMetricStatus[0]) {
-      const newMetricStatus = metric[1];
-      //if the checkedStatus are not the same
-      if (currentMetricStatus[0].checkedStatus !== newMetricStatus) {
-        //We update the DB instance
-        await UptimeCheck.findOneAndUpdate(
-          { checkedId: metric[0] },
-          { checkedStatus: newMetricStatus },
-          { new: true }
-        );
-        checksUpdated = true;
+    try {
+      const currentMetricStatus = await UptimeCheck.find({
+        checkedId: metric[0],
+      });
+      //if there is an instance
+      if (currentMetricStatus[0]) {
+        const newMetricStatus = metric[1];
+        //if the checkedStatus are not the same
+        if (currentMetricStatus[0].checkedStatus !== newMetricStatus) {
+          //We update the DB instance
+          await UptimeCheck.findOneAndUpdate(
+            { checkedId: metric[0] },
+            { checkedStatus: newMetricStatus },
+            { new: true }
+          );
+          checksUpdated = true;
+        }
       }
+    } catch (error) {
+      console.log(error);
     }
   }
   //If we updated the DB we return the new uptimeChecks
