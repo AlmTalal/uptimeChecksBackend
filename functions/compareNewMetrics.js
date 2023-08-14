1f26bcf993257d411ec6134f95394641ae7844be
Talal <alamtalal004@gmail.com>
2023-08-14 12:32:33 -0400

    Edited the algorithm to compare the currentUptime Checks with the new ones

--- functions/compareNewMetrics.js
+++ functions/compareNewMetrics.js
@@ -2,40 +2,43 @@ const { UptimeCheck } = require("../models/uptimeCheck");
 
 /**
  * Function that compares the new metrics with the ones that are stored in the DB
+ * The Algorithm is O(n*m) beacuase we have to search the newMetric Id inside the currenMetrics array so
+ * O(newMetrics * currentMetrics)
  */
 
-module.exports = async (newMetrics) => {
+module.exports = async (newMetrics, currentMetrics) => {
   let checksUpdated = false;
-  let currentUptimeChecks;
 
   for (metric of newMetrics) {
-    //We find its metric instance by the metric id
-    try {
-      const currentMetricStatus = await UptimeCheck.find({
-        checkedId: metric[0],
-      });
-      //if there is an instance
-      if (currentMetricStatus[0]) {
+    const newMetricId = metric[0];
+    for (currentMetric of currentMetrics) {
+      if (currentMetric.checkedId == newMetricId) {
         const newMetricStatus = metric[1];
         //if the checkedStatus are not the same
-        if (currentMetricStatus[0].checkedStatus != newMetricStatus) {
+        if (currentMetric.checkedStatus != newMetricStatus) {
           //We update the DB instance
-          await UptimeCheck.findOneAndUpdate(
-            { checkedId: metric[0] },
-            { checkedStatus: newMetricStatus },
-            { new: true }
-          );
-          checksUpdated = true;
+          try {
+            await UptimeCheck.findOneAndUpdate(
+              { checkedId: metric[0] },
+              { checkedStatus: newMetricStatus },
+              { new: true }
+            );
+            checksUpdated = true;
+          } catch (error) {
+            console.log(error);
+          }
         }
       }
-    } catch (error) {
-      console.log(error);
     }
   }
   //If we updated the DB we return the new uptimeChecks
   if (checksUpdated === true) {
-    currentUptimeChecks = await UptimeCheck.find();
-    return [currentUptimeChecks, checksUpdated];
+    try {
+      const currentUptimeChecks = await UptimeCheck.find();
+      return [currentUptimeChecks, checksUpdated];
+    } catch (error) {
+      console.log(error);
+    }
   }
   //else We return null and false
   return [null, false];
