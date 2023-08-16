0b3ac559bfc06bfe9c8fcb2dc4f8e2004ae3c13f
Talal <alamtalal004@gmail.com>
2023-08-16 15:41:29 -0400

    Added the method to generate a JWT token

--- models/user.js
+++ models/user.js
@@ -1,9 +1,15 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: require("find-config")("./.env") });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   userMail: { type: String, required: true },
   password: { type: String, required: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ userMail: this.userMail }, process.env.JWTPRIVATEKEY);
 };
 
 const User = mongoose.model("User", userSchema);
