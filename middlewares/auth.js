6577e78fea626ded5a49f26a2a87e5a1dbafdab7
Talal <alamtalal004@gmail.com>
2023-08-17 11:38:03 -0400

    fixed a user bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (token) => {
   const user = jwt.decode(token);
+  if (!user) return false;
   if (user.userMail) return true;
   return false;
 };
