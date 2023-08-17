513539f760ee24408447fe85678eb95ab0309f95
Talal <alamtalal004@gmail.com>
2023-08-17 11:38:32 -0400

    Removed unecesary code

--- socketEvents/users/authorization.js
+++ socketEvents/users/authorization.js
@@ -1,8 +1,7 @@
 const isAuthorized = require("../../middlewares/auth");
 
 module.exports = (socket) => {
-  socket.on("hasAuthorization", async (token, callback) => {
-    console.log("token");
+  socket.on("hasAuthorization", (token, callback) => {
     const isAuthed = isAuthorized(token);
     callback(isAuthed);
   });
