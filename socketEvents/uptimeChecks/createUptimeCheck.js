ac22e774ebe6b6c54832a189815c8669c61ab124
Talal <alamtalal004@gmail.com>
2023-08-18 16:08:41 -0400

    Added the authorization and callbacks to the create uptime check function

--- socketEvents/uptimeChecks/createUptimeCheck.js
+++ socketEvents/uptimeChecks/createUptimeCheck.js
@@ -1,19 +1,25 @@
+const authorize = require("../../middlewares/auth");
 const {
   validateUptimeCheck,
   UptimeCheck,
 } = require("../../models/uptimeCheck");
 
 module.exports = (socket) => {
-  socket.on("createUptimeCheck", async (newUptimeCheck) => {
+  socket.on("createUptimeCheck", async (newUptimeCheck, token, callback) => {
+    //Authorize
+    if (!authorize(token)) {
+      callback("error", "You are not authorized to do this operation");
+      return;
+    }
     //Validates the newUptimeCheck
     const error = validateUptimeCheck(newUptimeCheck);
     //If there is an error it emit it an return
     if (error) {
-      socket.emit("CreateUptimeCheckStatus", error.details[0].message);
+      callback("error", error.details[0].message);
       return;
     }
 
-    //It tries to create an uptime check, if it's succesfull emits true else emits false
+    //It tries to create an uptime check, if it's succesfull emits "success" else emits "error"
     try {
       const uptimeCheck = new UptimeCheck({
         checkedUrl: newUptimeCheck.checkedUrl,
@@ -22,12 +28,9 @@ module.exports = (socket) => {
         checkedStatus: newUptimeCheck.checkedStatus,
       });
       await uptimeCheck.save();
-      socket.emit("CreateUptimeCheckStatus", true);
+      callback("success", "Created the uptime check");
     } catch (error) {
-      socket.emit(
-        "CreateUptimeCheckStatus",
-        "Sorry, there was an error. Please retry"
-      );
+      callback("error", "Sorry, there was an error. Please retry");
     }
   });
 };
